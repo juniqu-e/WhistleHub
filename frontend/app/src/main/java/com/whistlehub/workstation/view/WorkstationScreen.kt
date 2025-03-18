a2b9429f3fae51df216e924771c2aa553d1a4702
vrooming13 <vrooming13@gmail.com>
2025-03-18 17:31:07 +0900

    feat : MainActivity Layout 설정 - DAW 화면 일 때만 Landscape - systemPadding 고려 한 바텀 네비게이션 설정

--- frontend/app/src/main/java/com/whistlehub/workstation/view/WorkstationScreen.kt
+++ frontend/app/src/main/java/com/whistlehub/workstation/view/WorkstationScreen.kt
@@ -1,20 +1,24 @@
 package com.whistlehub.workstation.view
 
 import android.app.Activity
-import android.content.pm.ActivityInfo
 import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.whistlehub.workstation.viewmodel.WorkStationViewModel
-
 
 
 @Composable
@@ -22,6 +26,7 @@ fun WorkStationScreen(navController: NavController) {
     val activity = LocalActivity.current as? Activity
     val viewModel: WorkStationViewModel = hiltViewModel()
     //Like React - useEffect
+    /*
     DisposableEffect(Unit) {
         // 원래 orientation 저장
         val originalOrientation = activity?.requestedOrientation
@@ -33,16 +38,26 @@ fun WorkStationScreen(navController: NavController) {
                 ?: ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
         }
     }
-
-
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+     */
+    Box(
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.displayCutout)
+            .background(Color.LightGray)
     ) {
-        Text(text = "Special Full Screen View in Landscape")
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Special Full Screen View in Landscape")
+        }
+
+        Box(modifier = Modifier.align(Alignment.BottomCenter)) {
+            viewModel.bottomBarProvider.WorkStationBottomBar()
+        }
     }
 
-    viewModel.bottomBarProvider.WorkStationBottomBar()
+
 }
