894254e62b25c9591ef81d7206d32240eb385570
wavewwave20 <wavewwave20@gmail.com>
2025-03-20 17:48:36 +0900

    feat: 플레이리스트 crud

--- backend/src/main/java/com/ssafy/backend/playlist/dto/ModifyPlaylistRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/playlist/dto/ModifyPlaylistRequestDto.java
@@ -4,17 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreatePlaylistRequestDto {
+public class ModifyPlaylistRequestDto {
+    private int playlistId;
     private String name;
     private String description;
-    private MultipartFile image; // file
-    private List<Integer> trackIds;
 }
