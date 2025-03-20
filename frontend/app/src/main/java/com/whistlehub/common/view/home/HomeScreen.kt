edcf9e13281b5159edb0bb0cfe51f460cc9a16d4
김세희 <sehee4329@gmail.com>
2025-03-20 10:23:32 +0900

    feat: TrackListRow, TrackListColumn 구성

--- frontend/app/src/main/java/com/whistlehub/common/view/home/HomeScreen.kt
+++ frontend/app/src/main/java/com/whistlehub/common/view/home/HomeScreen.kt
@@ -2,17 +2,21 @@ package com.whistlehub.common.view.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.whistlehub.common.view.copmonent.TrackItem
+import com.whistlehub.common.view.copmonent.TrackListColumn
+import com.whistlehub.common.view.copmonent.TrackListRow
 import com.whistlehub.common.view.theme.Typography
 import com.whistlehub.playlist.viewmodel.TrackPlayViewModel
 
+@OptIn(ExperimentalLayoutApi::class)
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun HomeScreen() {
@@ -23,15 +27,16 @@ fun HomeScreen() {
     trackPlayViewModel.loadTracks()
 
     // 트랙 리스트 UI
+    val trackList = trackPlayViewModel.trackList.collectAsState(initial = emptyList())
+
     Column {
         Text("홈화면", Modifier.fillMaxWidth(), style = Typography.displaySmall, textAlign = TextAlign.Center)
-        trackPlayViewModel.trackList.value.forEach { track ->
-            TrackItem(track)
-        }
         Button({
             trackPlayViewModel.stopTrack()
         }) {
             Text("정지")
         }
+        TrackListColumn(trackList = trackList)
+        TrackListRow(trackList = trackList)
     }
 }
