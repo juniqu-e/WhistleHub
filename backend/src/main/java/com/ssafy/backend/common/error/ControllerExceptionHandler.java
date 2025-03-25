6aa8789beb13a2b4abf5f0fd20a359fae6c63ec0
HEO-hyunjun <adjl1346@gmail.com>
2025-03-26 02:21:01 +0900

    feat: 비밀번호 찾기 기능 추가
    - 비밀번호 찾기 요청 처리하는 API 추가
    - 이메일과 아이디 일치 여부 확인하는 로직 구현
    - 임시 비밀번호 생성 및 이메일 전송 기능 추가
    - 관련 예외 처리 클래스 생성

--- backend/src/main/java/com/ssafy/backend/common/error/ControllerExceptionHandler.java
+++ backend/src/main/java/com/ssafy/backend/common/error/ControllerExceptionHandler.java
@@ -131,6 +131,13 @@ public class ControllerExceptionHandler {
                 .build();
     }
 
+    @ExceptionHandler(NotMatchIdAndEmailException.class)
+    public ApiResponse<?> notMatchIdAndEmailHandler(NotMatchIdAndEmailException e) {
+        return new ApiResponse.builder<Object>()
+                .errorStatus(ResponseType.NOT_MATCH_ID_AND_EMAIL)
+                .build();
+    }
+
     // 이외의 정의되지 않은 서버 에러처리
     @ExceptionHandler(Exception.class)
     public ApiResponse<?> serverErrorHandler(Exception e) {
