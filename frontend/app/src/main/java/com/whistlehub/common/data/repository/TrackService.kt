f0c82ea757cf593f046c8c1d05f3b54b393e86af
김세희 <sehee4329@gmail.com>
2025-03-26 09:13:24 +0900

    refactor: Service parameter 포맷 변경

--- frontend/app/src/main/java/com/whistlehub/common/data/repository/TrackService.kt
+++ frontend/app/src/main/java/com/whistlehub/common/data/repository/TrackService.kt
@@ -23,28 +23,31 @@ class TrackService @Inject constructor(
 ) : ApiRepository() {
     // 트랙 상세 조회
     suspend fun getTrackDetail(
-        token: String, trackId: String
+        token: String, 
+        trackId: String
     ): ApiResponse<TrackResponse.GetTrackDetailResponse> {
         return executeApiCall { trackApi.getTrackDetail(token, trackId) }
     }
 
     // 트랙 정보 수정
     suspend fun updateTrack(
-        token: String, request: TrackRequest.UpdateTrackRequest
+        token: String, 
+        request: TrackRequest.UpdateTrackRequest
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.updateTrack(token, request) }
     }
 
     // 트랙 삭제
     suspend fun deleteTrack(
-        token: String, trackId: String
+        token: String, 
+        trackId: String
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.deleteTrack(token, trackId) }
     }
 
     // 트랙 재생 요청
     suspend fun playTrack(
-        token: String = "tempToken",
+        token: String = "tempToken",  // 안 넣는 경우 임시 문자열 전송
         trackId: String,
     ): ByteArray? {
         val response = trackApi.playTrack(token, trackId) // API 호출
@@ -57,91 +60,105 @@ class TrackService @Inject constructor(
 
     // 트랙 재생 카운트 증가
     suspend fun increasePlayCount(
-        token: String, request: TrackRequest.TrackPlayCountRequest
+        token: String, 
+        request: TrackRequest.TrackPlayCountRequest
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.increasePlayCount(token, request) }
     }
 
     // 플레이리스트에 트랙 추가
     suspend fun addTrackToPlaylist(
-        token: String, request: TrackRequest.AddTrackToPlaylistRequest
+        token: String, 
+        request: TrackRequest.AddTrackToPlaylistRequest
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.addTrackToPlaylist(token, request) }
     }
 
     // 트랙 레이어 조회
     suspend fun getTrackLayers(
-        token: String, trackId: String
+        token: String, 
+        trackId: String
     ): ApiResponse<List<TrackResponse.GetTrackLayer>> {
         return executeApiCall { trackApi.getTrackLayers(token, trackId) }
     }
 
     // 트랙 레이어 재생
     suspend fun playLayer(
-        token: String, layerId: String
+        token: String, 
+        layerId: String
     ): ApiResponse<TrackResponse.TrackLayerPlay> {
         return executeApiCall { trackApi.playLayer(token, layerId) }
     }
 
     // 트랙 좋아요 상태 조회
     suspend fun getTrackLikeStatus(
-        token: String, trackId: String
+        token: String, 
+        trackId: String
     ): ApiResponse<Boolean> {
         return executeApiCall { trackApi.getTrackLikeStatus(token, trackId) }
     }
 
     // 트랙 좋아요
     suspend fun likeTrack(
-        token: String, request: TrackRequest.LikeTrackRequest
+        token: String, 
+        request: TrackRequest.LikeTrackRequest
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.likeTrack(token, request) }
     }
 
     // 트랙 댓글 조회
     suspend fun getTrackComments(
-        token: String, trackId: String
+        token: String, 
+        trackId: String
     ): ApiResponse<List<TrackResponse.GetTrackComment>> {
         return executeApiCall { trackApi.getTrackComments(token, trackId) }
     }
 
     // 트랙 댓글 작성
     suspend fun createTrackComment(
-        token: String, request: TrackRequest.CreateCommentRequest
+        token: String, 
+        request: TrackRequest.CreateCommentRequest
     ): ApiResponse<Int> {
         return executeApiCall { trackApi.createTrackComment(token, request) }
     }
 
     // 트랙 댓글 수정
     suspend fun updateTrackComment(
-        token: String, request: TrackRequest.UpdateCommentRequest
+        token: String, 
+        request: TrackRequest.UpdateCommentRequest
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.updateTrackComment(token, request) }
     }
 
     // 트랙 댓글 삭제
     suspend fun deleteTrackComment(
-        token: String, commentId: String
+        token: String, 
+        commentId: String
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.deleteTrackComment(token, commentId) }
     }
 
     // 트랙 검색
     suspend fun searchTracks(
-        token: String, request: TrackRequest.SearchTrackRequest
+        token: String, 
+        request: TrackRequest.SearchTrackRequest
     ): ApiResponse<List<TrackResponse.SearchTrack>> {
         return executeApiCall { trackApi.searchTracks(token, request) }
     }
 
     // 트랙 신고
     suspend fun reportTrack(
-        token: String, request: TrackRequest.ReportTrackRequest
+        token: String, 
+        request: TrackRequest.ReportTrackRequest
     ): ApiResponse<Unit> {
         return executeApiCall { trackApi.reportTrack(token, request) }
     }
 
     // 트랙 이미지 업로드
     suspend fun uploadTrackImage(
-        token: String, trackId: Int, image: MultipartBody.Part
+        token: String, 
+        trackId: Int, 
+        image: MultipartBody.Part
     ): ApiResponse<Unit> {
         val trackIdBody: RequestBody =
             trackId.toString().toRequestBody("text/plain".toMediaTypeOrNull())
