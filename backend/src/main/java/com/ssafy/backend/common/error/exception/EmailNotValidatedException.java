059165ac39eac76fcf73d2b3f08cd9c6c407e8ac
HEO-hyunjun <max8276@naver.com>
2025-03-26 14:33:27 +0900

    feat: 이메일 검증 로직 추가 및 예외 처리 구현 - 이메일 검증 미완료 시 EmailNotValidatedException 발생 - Redis에서 이메일 검증 키 삭제 로직 추가 - ResponseType에 EMAIL_NOT_VALIDATED 상태 추가 - Docker Compose 파일 수정 및 불필요한 db.yml 파일 삭제

---
+++ backend/src/main/java/com/ssafy/backend/common/error/exception/EmailNotValidatedException.java
@@ -0,0 +1,15 @@
+package com.ssafy.backend.common.error.exception;
+
+/**
+ * <pre>EmailNotValidatedException</pre>
+ * 이메일 검증 실패 예외
+ * @author 허현준
+ * @version 1.0
+ * @since 2025-03-26
+ */
+
+public class EmailNotValidatedException extends RuntimeException {
+    public EmailNotValidatedException() {
+        super();
+    }
+}
