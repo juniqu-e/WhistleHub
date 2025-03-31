f5bb91ea0b48bc0638242c55c0706573d0a9db22
byeongju <bottleaprk28@gmail.com>
2025-03-31 16:38:26 +0900

    fix: 트랙 검색 수정

--- backend/src/main/java/com/ssafy/backend/mysql/repository/TrackRepository.java
+++ backend/src/main/java/com/ssafy/backend/mysql/repository/TrackRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Repository
 public interface TrackRepository extends JpaRepository<Track, Integer> {
     List<Track> findAllByTitleContains(String title, PageRequest pageRequest);
+    List<Track> findAllByTitleContains(String title);
 
     List<Track> findByMemberIdAndVisibility(int memberId, boolean visibility, PageRequest pageRequest);
     List<Track> findByMemberId(int memberId, PageRequest pageRequest);
