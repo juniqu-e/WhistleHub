39e139cff641d083e6861ad92b4d92fa3f0c1ce5
byeongju <bottleaprk28@gmail.com>
2025-03-31 20:30:53 +0900

    feat: 트랙 임포트 작성 및 서비스 역할 분리

--- backend/src/main/java/com/ssafy/backend/track/dto/response/TrackSearchResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/track/dto/response/TrackSearchResponseDto.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class TrackSearchInfoDto {
+public class TrackSearchResponseDto {
     int trackId;
     String title;
     String nickname;
