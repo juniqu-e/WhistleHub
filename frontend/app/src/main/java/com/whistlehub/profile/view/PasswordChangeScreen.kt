7a3e628844445fb434168a873c7b9b2121fae3ff
Junik <zz262zz@naver.com>
2025-04-04 09:00:30 +0900

    feat: 빌드 에러 수정

--- frontend/app/src/main/java/com/whistlehub/profile/view/PasswordChangeScreen.kt
+++ frontend/app/src/main/java/com/whistlehub/profile/view/PasswordChangeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
