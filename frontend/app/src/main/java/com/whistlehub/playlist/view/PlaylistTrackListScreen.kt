9794ca84ad37f70fb04eefd4521cb3c3a1492cd1
김세희 <sehee4329@gmail.com>
2025-04-07 16:10:36 +0900

    fix: playlist track 임시복구...

--- frontend/app/src/main/java/com/whistlehub/playlist/view/PlaylistTrackListScreen.kt
+++ frontend/app/src/main/java/com/whistlehub/playlist/view/PlaylistTrackListScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -185,12 +186,13 @@ fun PlaylistTrackListScreen(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(10.dp)
                 ) {
-                    TrackItemRow(
-                        track = track,
-                        workStationViewModel = workStationViewModel,
-                        navController = navController,
-                        needMoreView = true
-                    )
+                    Box(Modifier.weight(1f)) {
+                        TrackItemRow(
+                            track,
+                            workStationViewModel = workStationViewModel,
+                            navController = navController,
+                        )
+                    }
                 }
             }
             item {
